df9609a941a11389fb75511d4157bdde10933938
Jin Li <jinl@microsoft.com>
2015-08-18 18:59:18 -0700

    Initial Scaffold for the Hub project. Disable unittest when it is not available.

---
+++ docs/content/index.fsx
@@ -0,0 +1,61 @@
+(*** hide ***)
+// This block of code is omitted in the generated HTML documentation. Use 
+// it to define helpers that you do not want to show in the documentation.
+#I "../../bin"
+
+(**
+VMHub
+======================
+
+Documentation
+
+<div class="row">
+  <div class="span1"></div>
+  <div class="span6">
+    <div class="well well-small">
+      The package runs VM Hub gateway. 
+    </div>
+  </div>
+  <div class="span1"></div>
+</div>
+
+Example
+-------
+
+This example demonstrates using a function defined in this sample library.
+
+*)
+
+
+// Examples TO-DO
+
+(**
+Some more info
+
+Samples & documentation
+-----------------------
+
+ * [Paper](paper/VMHub_1507.pdf) contains a paper that describes the technical components of the project. 
+
+ * [Tutorial](tutorial.html) contains a further explanation of this sample library.
+
+ * [API Reference](reference/index.html) contains automatically generated documentation for all types, modules
+   and functions in the library. This includes additional brief samples on using most of the
+   functions.
+ 
+Contributing and copyright
+--------------------------
+
+The project is hosted on [GitHub][gh] where you can [report issues][issues], fork 
+the project and submit pull requests. If you're adding a new public API, please also 
+consider adding [samples][content] that can be turned into a documentation. You might
+also want to read the [library design notes][readme] to understand how it works.
+
+For more information see the [License file][license] in the GitHub repository. 
+
+  [content]: https://github.com/MSRCCS/Hub/tree/master/docs/content
+  [gh]: https://github.com/MSRCCS/Hub
+  [issues]: https://github.com/MSRCCS/Hub/issues
+  [readme]: https://github.com/MSRCCS/Hub/blob/master/README.md
+  [license]: https://github.com/MSRCCS/Hub/blob/master/LICENSE.txt
+*)
